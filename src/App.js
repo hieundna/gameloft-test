a5764964e55168a64821354ac9122b9c208151bf
hieundna <hieungdi98@gmail.com>
2021-03-30 07:52:35 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div>
+      <div className="header">
+        <div className="logo">
+          <img src='./gameloft-logo.png' alt='gameloft'/>
+        </div>
+        <div className='hamberger'>
+          <span className='berger'></span>
+        </div>
+      </div>
+      <div className='hotspot-banner'>
+        <div className='triangle-left'>
+          <div className="inner-triangle"></div>
+        </div>
+      </div>
+      <div className='store-buttons'>
+
+      </div>
     </div>
   );
 }
